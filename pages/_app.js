0909e2a25e4ba7c6b1c36f3bc8729df3d778dbd5
Duy. Bui Quang - CMC Global DE1 <bqduy1@cmcglobal.vn>
2022-12-12 17:14:27 +0700

    intial project

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import "tailwindcss/tailwind.css";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css';
+
+const Home = () => {
+  return (
+    <>     
+        <h1 className={styles.container}>Hello World</h1>
+    </>
+  );
+}
+
+export default Home;
